a5e3d68d81ef80247182493ab42672e024b55ab5
Pepe Becker <mail@pepebecker.com>
2024-02-08 08:58:57 -0700

    Sort treesitter installs
    Signed-off-by: Pepe Becker <mail@pepebecker.com>

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,11 +6,13 @@ return {
     opts.incremental_selection.keymaps.node_decremental = "<bs>"
     opts.incremental_selection.keymaps.scope_incremental = "<Enter>"
     vim.list_extend(opts.ensure_installed, {
-      "markdown",
-      "markdown_inline",
       "glsl",
       "lua",
+      "markdown",
+      "markdown_inline",
       "odin",
+      "slint",
+      "swift",
       "zig",
     })
   end,
